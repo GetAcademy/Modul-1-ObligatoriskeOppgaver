05c3b7c3b1f687fcb7a2fcf306c571cda0866232
waninko <43968374+waninko@users.noreply.github.com>
2021-03-09 07:28:48 +0100

    Obligatorisk Oppgave 1
    - oppgavebeskrivelse + forslag - eksempeltester (en for controllerfunk og en for ren funk)
    - view+model i index.html

---
+++ Obligatorisk Oppgave 1/funksjoner.js
@@ -0,0 +1,13 @@
+//eksempel ren funksjon, kun for å vise opp mot test i test dokumentet. Denne funksjonen kan fjernes
+function gangeEtTallMedSegSelv(nummer){
+    //Ganger nummer med seg selv og returnerer den verdien
+    return nummer * nummer;
+}
+
+//eksempel controller funksjon (altså en som endrer på modellen)  
+function økMedEn(){
+    test++;
+    updateVeiw();
+}
+
+//controller (her putter du controller funksjonene i mv(c) )
